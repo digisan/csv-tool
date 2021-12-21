49417e8007902d2d6ddf9af0f356995326e38f6c
Qing Miao <digisanQM@outlook.com>
2021-12-21 11:16:05 +1100

    + scan

---
+++ misc.go
@@ -0,0 +1,84 @@
+package csvtool
+
+import (
+	"fmt"
+	"strings"
+
+	gio "github.com/digisan/gotk/io"
+	lk "github.com/digisan/logkit"
+)
+
+var (
+	sContains     = strings.Contains
+	sReplaceAll   = strings.ReplaceAll
+	sHasPrefix    = strings.HasPrefix
+	sHasSuffix    = strings.HasSuffix
+	sTrimSuffix   = strings.TrimSuffix
+	sTrim         = strings.Trim
+	sJoin         = strings.Join
+	fSf           = fmt.Sprintf
+	fEf           = fmt.Errorf
+	warnOnErr     = lk.WarnOnErr
+	failP1OnErr   = lk.FailP1OnErr
+	failOnErrWhen = lk.FailOnErrWhen
+	mustCreateDir = gio.MustCreateDir
+)
+
+func isWrappedWith(s, prefix, suffix string) bool {
+	return sHasPrefix(s, prefix) && sHasSuffix(s, suffix)
+}
+
+func isWrappedWithSQ(s string) bool {
+	return isWrappedWith(s, "'", "'")
+}
+
+func isWrappedWithDQ(s string) bool {
+	return isWrappedWith(s, "\"", "\"")
+}
+
+func tryWrap(s, prefix, suffix string) string {
+	if !isWrappedWith(s, prefix, suffix) {
+		return prefix + s + suffix
+	}
+	return s
+}
+
+func tryWrapWithSQ(s string) string {
+	return tryWrap(s, "'", "'")
+}
+
+func tryWrapWithDQ(s string) string {
+	return tryWrap(s, "\"", "\"")
+}
+
+////////////////////////////////////////////////
+
+func tryStrip(s string, n int) string {
+	if n < 0 {
+		panic("[n] must >= 0")
+	}
+	if len(s) >= 2*n {
+		return s[n : len(s)-n]
+	}
+	return s
+}
+
+func tryStripSQ(s string) string {
+	if isWrappedWithSQ(s) {
+		return tryStrip(s, 1)
+	}
+	return s
+}
+
+func tryStripDQ(s string) string {
+	if isWrappedWithDQ(s) {
+		return tryStrip(s, 1)
+	}
+	return s
+}
+
+////////////////////////////////////////////////
+
+func isBlank(s string) bool {
+	return len(sTrim(s, " \t\n")) == 0
+}
